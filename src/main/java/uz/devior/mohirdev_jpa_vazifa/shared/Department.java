2b6a388a4c0caf1bd26e30411ac053a0d13ae3b0
odilhon85 <i.o.r@inbox.ru>
2023-10-29 09:12:56 +0500

    Enum value enabled

--- src/main/java/uz/devior/mohirdev_jpa_vazifa/shared/Department.java
+++ src/main/java/uz/devior/mohirdev_jpa_vazifa/shared/Department.java
@@ -1,5 +1,7 @@
 package uz.devior.mohirdev_jpa_vazifa.shared;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Department {
     HR("Xodimlarni boshqarish bo'limi"),
     CLIENT("Mijozlar bilan ishlash bo'limi");
@@ -7,5 +9,21 @@ public enum Department {
     final String departmentName;
     Department(String departmentName) {
         this.departmentName = departmentName;
+    }
+
+    public String getdepartmentName() {
+        return this.departmentName;
+    }
+
+    @JsonCreator
+    public static Department getDepartmentFromCode(String value) {
+
+        for (Department dep : Department.values()) {
+            if (dep.getdepartmentName().equals(value)) {
+                return dep;
+            }
+        }
+
+        return null;
     }
 }
--- src/main/java/uz/devior/mohirdev_jpa_vazifa/shared/Position.java
+++ src/main/java/uz/devior/mohirdev_jpa_vazifa/shared/Position.java
@@ -1,5 +1,7 @@
 package uz.devior.mohirdev_jpa_vazifa.shared;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Position {
     DIRECTOR("Direktor"),
     DEPARTMENT_HEAD("Bo'lim boshlig'i"),
@@ -8,5 +10,21 @@ public enum Position {
     final String position;
     Position(String position){
         this.position = position;
+    }
+
+    public String getPosition() {
+        return this.position;
+    }
+
+    @JsonCreator
+    public static Position getPositionFromCode(String value) {
+
+        for (Position position : Position.values()) {
+            if (position.getPosition().equals(value)) {
+                return position;
+            }
+        }
+
+        return null;
     }
 }
